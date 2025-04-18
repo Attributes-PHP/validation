0f447b09e695ba5b4be102a231498c57657b014b
matapatos <andre_gil22@hotmail.com>
2025-04-18 19:04:53 +0100

    tests: Add strict and invalid nested tests

--- tests/Integration/NestedFieldsTest.php
+++ tests/Integration/NestedFieldsTest.php
@@ -12,12 +12,13 @@ declare(strict_types=1);
 
 namespace Attributes\Validation\Tests\Integration;
 
+use Attributes\Validation\Exceptions\ValidationException;
 use Attributes\Validation\Tests\Integration\Models\Nested as Models;
 use Attributes\Validation\Validator;
 use DateTime;
 
-test('Valid nested', function () {
-    $validator = new Validator;
+test('Valid nested', function (bool $isStrict) {
+    $validator = new Validator(strict: $isStrict);
     $data = [
         'profile' => [
             'firstName' => 'Andre',
@@ -44,12 +45,41 @@ test('Valid nested', function () {
         ->toHaveProperty('id', 1);
 
 })
+    ->with([true, false])
     ->group('validator', 'nested');
 
-// test('Invalid bool', function ($value) {
-//    $validator = new Validator;
-//    $validator->validate(['value' => $value], new Models\Boolean);
-// })
-//    ->with('invalid bool')
-//    ->throws(ValidationException::class, 'Invalid data')
-//    ->group('validator', 'nested');
+test('Invalid strict nested', function () {
+    $validator = new Validator(strict: true);
+    $data = [
+        'profile' => [
+            'firstName' => 'Andre',
+            'lastName' => 'Gil',
+            'post' => [
+                'id' => 1.98,
+                'title' => 'How to validate data with classes in PHP',
+            ],
+        ],
+        'userType' => 'moderator',
+        'createdAt' => '2025-03-28T16:00:00+00:00',
+    ];
+    $validator->validate($data, new Models\User);
+})
+    ->throws(ValidationException::class, 'Invalid data')
+    ->group('validator', 'nested');
+
+test('Invalid nested', function (bool $isStrict) {
+    $validator = new Validator(strict: $isStrict);
+    $data = [
+        'profile' => [
+            'firstName' => 'Andre',
+            'post' => [
+                'id' => 'Invalid integer',
+            ],
+        ],
+        'userType' => 'moderator',
+    ];
+    $validator->validate($data, new Models\User);
+})
+    ->with([true, false])
+    ->throws(ValidationException::class, 'Invalid data')
+    ->group('validator', 'nested');
