2f8b02175a33506e5e4f30c081e104f1635e28c6
matapatos <andre_gil22@hotmail.com>
2025-04-23 09:49:42 +0100

    tests: Add additional validation rules via attributes in nested models

--- tests/Integration/Models/Nested/Post.php
+++ tests/Integration/Models/Nested/Post.php
@@ -3,13 +3,16 @@
 namespace Attributes\Validation\Tests\Integration\Models\Nested;
 
 use DateTime;
+use Respect\Validation\Rules as Rules;
 
 class Post
 {
     public int|string $id;
 
+    #[Rules\NotEmpty]
     public string $title;
 
+    #[Rules\When(new Rules\IntVal, new Rules\Min(0))]
     public int|DateTime $published;
 
     public function __construct(?DateTime $published = null)
