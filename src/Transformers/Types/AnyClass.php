8c461c03f85e88517e440cfe7018d9320569ef55
matapatos <andre_gil22@hotmail.com>
2025-04-29 08:07:54 +0100

    chore: Enable strict types

--- src/Transformers/Types/AnyClass.php
+++ src/Transformers/Types/AnyClass.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Holds interface for casting a given value
  */
--- src/Validator.php
+++ src/Validator.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Attributes\Validation;
 
 use Attributes\Validation\Exceptions\BaseException;
