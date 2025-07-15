95d33ee1dc8f5bbc68ce1e51f60149272316c94a
matapatos <andre_gil22@hotmail.com>
2025-07-15 08:44:51 +0100

    chore: Add missing ArrayAccess type-hint to validate

--- src/Validatable.php
+++ src/Validatable.php
@@ -14,12 +14,12 @@ use Attributes\Validation\Exceptions\ValidationException;
 interface Validatable
 {
     /**
-     * @param  array  $data  - Data to be validated
+     * @param  array|ArrayAccess  $data  - Data to be validated
      * @param  object  $model  - Model to validate against
      *
      * @throws ValidationException - If the validation fails
      */
-    public function validate(array $data, object $model): object;
+    public function validate(array|ArrayAccess $data, object $model): object;
 
     /**
      * @param  array|ArrayAccess  $data  - Data to be validated
